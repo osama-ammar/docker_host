e74776b816d3ecc8e9dacacce8d69431dad29514
Osama Ammar <ossssamar@gmail.com>
2024-04-18 23:58:41 +0200

    working scikit model

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 # Use an official Python runtime as a parent image\
 #for cuda version use , otherwise ---> FROM python:3.9-slim
-FROM nvidia/cuda:11.1.1-cudnn8-runtime-ubuntu20.04
+#FROM nvidia/cuda:11.1.1-cudnn8-runtime-ubuntu20.04
+
+# Use an official Python runtime as a parent image
+FROM python:3.9-slim
 
 # Set the working directory in the container
-WORKDIR /app
+WORKDIR /usr/src/app
 
 # Copy the current directory contents into the container at /usr/src/app
 COPY . .
@@ -11,6 +14,5 @@ COPY . .
 # Install any needed packages specified in requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
-#RUN  pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-# Run app.py when the container launches
-CMD ["python", "./app.py"]
+# Run app.py when the container launches #? executed when you run the container
+CMD ["python", "./sklearn_model.py"]
--- docker_operations.bat
+++ docker_operations.bat
@@ -1,3 +1,7 @@
+REM image : 
+REM container :
+REM docker :
+REM build image|container  means -->
 
 REM building a docker image for this project and naming it as ml-docker-app  
 REM you should run docker engine first , before build
@@ -9,6 +13,14 @@ docker run -it --rm ml-docker-app .
 REM Once you have a Docker Hub account, you need to log in through the command line on your local machine. Open your terminal and run:
 docker login
 
+
+REM Before you can push an image to Docker Hub, it must be tagged with your Docker Hub username. If you don’t tag it correctly, Docker will not know where to push the image.
 REM tag your docker image
 docker tag ml-docker-app username/ml-docker-app
 
+REM To push the image to Docker Hub, use the docker push command followed by the name of the image you want to push:
+docker push username/ml-docker-app
+
+REM Check the Pushed Container Image on Docker Hub
+REM You can go to your Docker Hub repository and see the recently pushed image.
+REM You have successfully containerized a simple machine learning application, pushed it to Docker Hub, and made it available to be pulled and run from anywhere.
